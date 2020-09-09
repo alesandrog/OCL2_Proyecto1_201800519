df61566af3fa3696744290d827f6340e4cde632f
Alesandro Gonzalez <nelsonalesandrog@gmail.com>
2020-09-08 23:51:06 -0600

    [feat] forIn basico

---
+++ Instruction/ForIn.ts
@@ -0,0 +1,46 @@
+import { Instruction } from "../Abstract/Instruccion";
+import { Expresion } from "../Abstract/Expresion";
+import { Entorno } from "../Symbol/Entorno";
+import { Tipo } from "../Abstract/Retorno";
+import { Error_ } from "../Error/Error";
+import { BloqueInstrucciones } from "./BloqueInstrucciones";
+import { Break } from "./Break";
+import { Continue } from "./Continue";
+import { Asignacion } from "./Asignacion";
+import { Declaration } from "./Declaracion";
+
+export class ForIn extends Instruction {
+ 
+    private declaracion : Declaration;
+    private arreglo: Expresion;
+    private limite : number;
+    private code: BloqueInstrucciones;
+
+  constructor( declaracion : Declaration , arreglo : Expresion, code : BloqueInstrucciones, linea: number, columna: number) {
+    super(linea, columna);
+    this.declaracion = declaracion;
+    this.arreglo = arreglo;
+    this.code = code;
+    this.limite = 0;
+  }
+
+  public execute(entorno: Entorno) {
+    
+    //Declarar la variable en un nuevo entorno
+
+    const iterador = this.arreglo.execute(entorno);
+
+    if(iterador.tipo != Tipo.ARRAY)
+        return;    //error
+
+    //establecer limite del for
+    this.limite = iterador.value.length;
+    
+    for(let i = 0; i < this.limite; i++){
+        let element = iterador.value[i];
+        entorno.guardarVariable(this.declaracion.id, element.value, element.tipo, this.declaracion.variable);
+        this.code.execute(entorno);    
+    }
+    
+  }
+}
