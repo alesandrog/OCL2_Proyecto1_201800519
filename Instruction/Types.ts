6ec265d0beeeadbba4791bbf0908af65e5c9ef1c
Alesandro Gonzalez <nelsonalesandrog@gmail.com>
2020-09-10 21:57:37 -0600

    [feat] validaciones

--- Instruction/Types.ts
+++ Instruction/Types.ts
@@ -3,6 +3,9 @@ import { Retorno, Tipo } from "../Abstract/Retorno";
 import { Entorno } from "../Symbol/Entorno";
 import { AtributoType } from "../Expression/AtributoType";
 import { Instruction } from "../Abstract/Instruccion";
+import { AccesoTipoType } from "../Expression/AccesoTipoType";
+import { strict } from "assert";
+import { Error_ } from "../Error/Error";
 
 export class Type extends Instruction{
     
@@ -11,21 +14,55 @@ export class Type extends Instruction{
     public linea : number;
     public columna : number;
     private id : string;
+    private tipo : AccesoTipoType
 
-    constructor(id: string ,valor : Array<AtributoType> , linea : number, columna: number){
+    constructor(id: string ,valor : Array<AtributoType> , tipo : AccesoTipoType, linea : number, columna: number){
         super(linea, columna);
         this.id = id;
         this.atributos = valor;
+        this.tipo = tipo;
         this.linea = linea;
         this.columna = columna;
     }
 
     public execute(entorno : Entorno){
+        //TODO validar que se encuentre el type
 
+        //Verificar que no se recibe algo diferente a un ID
+        if((this.tipo instanceof AccesoTipoType) == false)
+            throw new Error_(this.linea, this.columna, 'Semantico', this.tipo + ' No asignable a Type');
+        
+        //Ejecutar los atributos recibidos y agregarlos al Map
         for(const instr of this.atributos){
             const exe = instr.execute(entorno);
             this.value.set(instr.id , exe);
         }
-        entorno.guardarVariable(this.id, this.value, Tipo.TYPE, true);
+
+        //Convertir map en array para verificar valores
+        let arr = [];    
+        for(const val of this.value){
+                arr.push(val);
+            }
+
+        //Buscar el tipo en el Map
+        const tipo = this.tipo.execute(entorno);
+        
+        //Validar que el tipo existe
+        if(tipo == null || tipo == undefined)
+            throw new Error_(this.linea, this.columna, 'Semantico', 'Type no definido');
+
+        
+        //Verificar que la cantidad de atributos recibidos sea igual a la cantidad esperada
+
+        if(arr.length != tipo.valor.length)
+            throw new Error_(this.linea, this.columna, 'Semantico', 'Atributos no coinciden con los definidos en ' + tipo.id);
+
+
+        //Comparar atributos recibidos con atributos definidos en el type
+       for(let i = 0; i < tipo.valor.length; i++)
+            if(tipo.valor[i].id != arr[i][0] || tipo.valor[i].tipo != arr[i][1].tipo)
+                throw new Error_(this.linea, this.columna, 'Semantico', 'Atributos no coinciden con los definidos en ' + tipo.id);
+        
+        entorno.guardarVariable(this.id, this.value, tipo.tipo, true);
     }    
 }
