8a25b29b9862d0c573654cbfe5c81b1938faaa99
Alesandro Gonzalez <nelsonalesandrog@gmail.com>
2020-09-11 13:06:39 -0600

    [feat] impresion de arreglos

--- Instruction/Console.ts
+++ Instruction/Console.ts
@@ -1,7 +1,8 @@
 import { Instruction } from "../Abstract/Instruccion";
 import { Expresion } from "../Abstract/Expresion";
 import { Entorno } from "../Symbol/Entorno";
-
+import { Tipo, Retorno } from "../Abstract/Retorno";
+import { Arreglo2 } from "../Instruction/Arreglo2";
 export class Console extends Instruction{
 
     constructor(private value : Expresion, line : number, column : number){
@@ -9,7 +10,26 @@ export class Console extends Instruction{
     }
 
     public execute(entorno : Entorno) {
-        const value = this.value.execute(entorno);
-        console.log(value.value);
+        let value : any;
+        value = this.value.execute(entorno);
+
+        if(value.tipo == Tipo.ARRAY){
+            console.log(this.ejecutar(entorno, value).value);
+        }else{
+            console.log(value.value);
+        }
+        
+    }
+
+    public ejecutar(entorno : Entorno, arreglo : Retorno): Retorno {
+        let res = [];
+        for(const instr of arreglo.value){
+            if(instr.tipo == Tipo.ARRAY){
+                res.push(this.ejecutar(entorno, instr).value);
+            }else{
+                res.push(instr.value);
+            }
+        }
+        return { value : res, tipo : Tipo.ARRAY };
     }
 }
